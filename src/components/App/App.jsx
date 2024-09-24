d945260738fe3642e81dcfa9bdfb849c2be396b9
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-09-24 15:50:32 +0300

    Add Reset button

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,6 +4,7 @@ import Description from '../Description/Description';
 import Feedback from '../Feedback/Feedback';
 import Options from '../Options/Options';
 import { useState } from 'react';
+import Notification from '../Notification/Notification';
 
 const App = () => {
   const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
@@ -15,11 +16,21 @@ const App = () => {
     }));
   };
 
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
   return (
     <>
       <Description />
-      <Options updateFeedback={updateFeedback} />
-      <Feedback feedback={feedback} />
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback > 0 ? <Feedback feedback={feedback} /> : <Notification />}
     </>
   );
 };
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,9 +1,10 @@
-const Options = ({ updateFeedback }) => {
+const Options = ({ updateFeedback, totalFeedback, resetFeedback }) => {
   return (
     <div>
       <button onClick={() => updateFeedback('good')}>Good</button>
       <button onClick={() => updateFeedback('neutral')}>Neutral</button>
       <button onClick={() => updateFeedback('bad')}>Bad</button>
+      {totalFeedback > 0 && <button onClick={resetFeedback}>Reset</button>}
     </div>
   );
 };
