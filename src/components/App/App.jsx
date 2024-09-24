79f1f98619bd2f2ae73ca7da001f4dc30590f802
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-09-24 13:05:31 +0300

    Add Description component

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import './App.module.css';
+
+import Description from '../Description/Description';
+const App = () => {
+  return (
+    <>
+      <Description />
+    </>
+  );
+};
+
+export default App;
