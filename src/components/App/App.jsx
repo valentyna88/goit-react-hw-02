9cfb6a2d1d14a295f697045c2794a7bb36a8121a
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-09-24 20:42:38 +0300

    Update files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,11 +3,20 @@ import './App.module.css';
 import Description from '../Description/Description';
 import Feedback from '../Feedback/Feedback';
 import Options from '../Options/Options';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Notification from '../Notification/Notification';
 
 const App = () => {
-  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem('feedback');
+    return savedFeedback
+      ? JSON.parse(savedFeedback)
+      : { good: 0, neutral: 0, bad: 0 };
+  });
+
+  useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
 
   const updateFeedback = feedbackType => {
     setFeedback(prevFeedback => ({
